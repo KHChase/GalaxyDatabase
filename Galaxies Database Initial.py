f3ea195e53d890fd9998b0255df62188a378dc66
R1leyMP <148601021+R1leyMP@users.noreply.github.com>
2024-11-28 13:12:55 -0900

    Adding csv_to_hash to HashTable Class
    Check if this still works, I ran it in pyzo and it looks like it's running correctly

--- Galaxies Database Initial.py
+++ Galaxies Database Initial.py
@@ -21,21 +21,21 @@ class HashTable:
 
 #converts csv to hash table(s) to only have to load once
 
-def csv_to_hash(file_path, index_cols):
-    hash_tables = {col: HashTable() for col in index_cols}
-
-    with open(file_path, mode='r') as csv_file:
-        reader = csv.DictReader(csv_file)
-
-        for row in reader:
-            for col in index_cols:
-
-                #only insert if key is not None or empty
-                key = row.get(col)
-                if key:
-                    hash_tables[col].insert(key, row)
-
-    return hash_tables
+    def csv_to_hash(file_path, index_cols):
+        hash_tables = {col: HashTable() for col in index_cols}
+    
+        with open(file_path, mode='r') as csv_file:
+            reader = csv.DictReader(csv_file)
+    
+            for row in reader:
+                for col in index_cols:
+    
+                    #only insert if key is not None or empty
+                    key = row.get(col)
+                    if key:
+                        hash_tables[col].insert(key, row)
+    
+        return hash_tables
 
 
 #main function
