e58ffa26e2adb14352358cadde1af05003fbc201
KHChase <149190605+KHChase@users.noreply.github.com>
2024-12-01 02:08:33 -0900

    Update search.py
    Adds new search functions that do specific types of searches

--- search.py
+++ search.py
@@ -1,10 +1,9 @@
 '''
     searches
         colSearch - return data for one column
-        
         rangeSearch - return all galaxies with values in range
         matchSearch - return all galaxies with values that match
-        crossSearch - multiple of these
+        crossSearch - multiple overlapping rangeSearch and matchSearch
 '''
 
 
@@ -77,3 +76,58 @@ def rangeSearch(attribute, lower, upper, hash_tables):
             print(g)
                 
     return galaxies
+
+# searches data column for data within given value
+# returns list of galaxies that has value
+def matchSearch(attribute, target, hash_tables):
+    galaxies = []
+    data = hash_tables[attribute].column() # FIXME: KeyError
+
+    for i in data:
+        # since this is checking strings it should
+        if i == target:
+            for j in hash_tables[attribute].search(i):
+                galaxies.append(j)
+
+    return galaxies
+
+# returns all data from one column
+# not the same as returning the column, this is a List and not part of a HashTable
+def colSearch(attribute, hash_tables):
+    result = hash_tables[attribute].column()
+    vals = []
+    for i in result:
+        for j in hash_tables[attribute].search(i):
+            vals.append(i)
+    return vals
+
+# returns list of galaxies that fit multiple matchSearch / rangeSearch
+# user refines search in steps
+def crossSearch(hash_tables):
+    galaxies = colSearch("Galaxy", hash_tables)
+    exit = False
+    while not exit:
+        # FIXME: prompt user to do stuff
+        choice = int(input("range (1), match (2), or exit (-1): "))
+        # perform limiting search
+        match choice:
+            case 1:
+                attribute = input("attribute: ")
+                lower = float(input("lower bound: "))
+                upper = float(input("upper bound: "))
+                limit = rangeSearch(attribute, lower, upper, hash_tables)
+            case 2:
+                attribute = input("attribute: ")
+                target = input("target: ")
+                limit = matchSearch(attribute, target, hash_tables)
+            case -1:
+                exit = True
+                
+        # update limit_tables to reflect search
+        save = []
+        for i in limit:
+            if galaxies.count(i) > 0:
+                save.append(i)
+        galaxies = save
+    return galaxies
+
