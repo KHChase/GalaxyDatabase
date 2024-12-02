65ae29fedc1489b7b8b0bb1ca24a6f162a3804d9
KHChase <149190605+KHChase@users.noreply.github.com>
2024-12-01 22:53:16 -0900

    Update add_update.py

--- add_update.py
+++ add_update.py
@@ -12,7 +12,7 @@ def add_update(hash_tables, index_cols, file_path):
     for col in index_cols:
         hash_tables[col].insert(new_data[col], new_data)
 
-    written = write_to_csv(galaxy_name, file_path, hash_tables)
+    written = write_to_csv(galaxy_name, file_path, hash_tables, index_cols)
     
     if written:
         print(f"\nGalaxy '{galaxy_name}' has been added successfully.")
